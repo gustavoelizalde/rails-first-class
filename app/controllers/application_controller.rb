52682e49f588050aad4e08528880678ba0222de8
gustavoelizalde <gustavoelizalde@gmail.com>
2016-06-24 21:22:33 -0400

    Haciendo la variable publica en el controller Application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :public_variables
+
+  def public_variables
+  	@nombres = ["Gustavo", "Oscar", "Rodrigo", "Agustin", "Diego", "Seba", "Benja", "Enyel", "Gonzalo", "Alex" ]
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def index
-  	@nombres = ["Gustavo", "Oscar", "Rodrigo", "Agustin", "Diego", "Seba", "Benja", "Enyel", "Gonzalo", "Alex" ]
   end
 
   def about
