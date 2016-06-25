a72f5774a8d6be32c6db38d41a5f189cb656597d
gustavoelizalde <gustavoelizalde@gmail.com>
2016-06-24 21:12:36 -0400

    Uploading template with Foundation 6 gem installed

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def index
+  	@nombres = ["Gustavo", "Oscar", "Rodrigo", "Agustin", "Diego", "Seba", "Benja", "Enyel", "Gonzalo", "Alex" ]
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
